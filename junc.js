4a3aa0be2578000775b227226047af080b6ef247
Dimon Eir <deiramjani@gmail.com>
2021-06-18 20:01:10 -0700

    Callbacks and Error Handling 2

--- junc.js
+++
@@ -1,9 +0,0 @@
-const outerFn3 = () => {
-    const x=5;
-    const innerFn = () => console.log(x);
-    return innerFn;
-};
-
-const closureTest = outerFn3();
-
-closureTest();
--- rectangle.js
+++ rectangle.js
@@ -9,10 +9,8 @@ module.exports = (x, y, callback) => {
                 area: () => x*y
             }), 
             2000
-        )
+        );
     }
-}
+};
 
 
-perimeter = (x,y) => 2*(x+y);
-area = (x,y) => x*y;
